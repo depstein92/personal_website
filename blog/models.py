8a392a8e92658bd03173874a6c13604f06bf816e
Daniel Epstein <d.epstein92@gmail.com>
2019-02-27 14:44:42 -0500

    added additional styling

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
       date = models.DateField()
       body = models.TextField()
       image = models.ImageField(upload_to="images/")
+
+      def summary(self):
+          return  self.body[:100]
+
+      def pub_date_pretty(self):
+          return self.pub_date.strftime('%b %e %Y')
+
+      def __str__(self): # for django admin
+           return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+from .models import Blog
+
 def allblogs(request):
-    return render(request, 'blog/allblogs.html')
+    blogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {'blogs': blogs })
+
+def detail(request, blog_id):
+    return blog_id #will change 
