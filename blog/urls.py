264e4420b1e61ab12df1f16abe40e6c1b374f723
Daniel Epstein <d.epstein92@gmail.com>
2019-02-27 17:36:39 -0500

    added additional styling to home.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path('', views.allblogs, name="allblogs"),
     path('<int:blog_id>/', views.detail, name='detail')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
